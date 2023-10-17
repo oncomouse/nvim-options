8bd2798def5fd65f57b4110ec506b84271adb11f
oncomouse <oncomouse@gmail.com>
2023-10-17 15:01:25 -0500

    fix: config problems

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,5 +2,5 @@ require("config.options")
 
 return {
 	{ "folke/lazy.nvim", version = "*" },
-	{ "oncomouse/nvim-options", priority = 10000, lazy = false, config = true, cond = true, version = "*" }
+	{ "oncomouse/nvim-options", priority = 10000, lazy = false, cond = true, version = "*" }
 }
